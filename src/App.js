b3c375e9f746b28b3e7d72000fb0cda76f8b1b85
Anis-CS <anisurrhaman0133@gmail.com>
2024-08-28 21:33:26 +0600

    json data array for f

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Card from './component/Card';
+import React from 'react'
+import Card from './component/Card'
+import Data from './data.json'
+
+
 
 function App() {
+  
+  let items=[];
+  for(let x=0; x<Data.length; x++){
+    items.push(<Card titleText={Data[x].title} desText={Data[x].desc}/>)
+  }
+
   return (
+          // <div>
+          //     <h1 className="headingTitle">welcome world</h1>
+          //     <Card titleText="Top heading" desText="Lorem Ipsum is dummy1"/>
+          //     <Card titleText="Top Menu" desText="Lorem Ipsum is simply dummy2">
+          //     <Card titleText="Top Body" desText="Lorem Ipsum is simply dummy3">
+          // </div>
           <div>
               <h1 className="headingTitle">welcome world</h1>
-              <Card titleText="Top heading" desText="Lorem Ipsum is simply dummy1"/>
-              <Card titleText="Top Menu" desText="Lorem Ipsum is simply dummy2"/>
-              <Card titleText="Top Body" desText="Lorem Ipsum is simply dummy3"/>
+              {items}
           </div>
   );
     
